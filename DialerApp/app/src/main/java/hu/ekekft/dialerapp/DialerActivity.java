38645dba169b6136421048301021bc300a5a1411
kicsikoko <kiskoko5@gmail.com>
2016-10-13 17:02:03 +0200

    Start Dialer Activity with explicit intent

--- DialerApp/app/src/main/java/hu/ekekft/dialerapp/DialerActivity.java
+++ DialerApp/app/src/main/java/hu/ekekft/dialerapp/DialerActivity.java
@@ -2,6 +2,7 @@ package hu.ekekft.dialerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DialerActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class DialerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialer);
+
+        TextView numberText = (TextView) findViewById(R.id.number_textView);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String number = extras.getString(MainActivity.EXTRA_MESSAGE);
+            numberText.setText(number);
+        }else{
+            numberText.setText(R.string.no_number);
+        }
     }
 }
